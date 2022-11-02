6626b44838c07867c68be1434583f6ed4f13f771
hamahama59 <87935612+hamahama59@users.noreply.github.com>
2022-11-03 02:02:05 +0900

    antd 기본 적용

--- src/main/index.js
+++ src/main/index.js
@@ -22,46 +22,38 @@ function MainPage() {
   }, []);
   return (
     <div>
-      <div id="header">
-        <div id="header-area">
-          <img src="images/icons/logo.png" />
-        </div>
+      <div id="banner">
+        <img src="images/banners/banner1.png" />
       </div>
-      <div id="body">
-        <div id="banner">
-          <img src="images/banners/banner1.png" />
-        </div>
-        <StudyingComponent />
-        {
-          //오오오오오 신기하다 된다!!!!!!!!!!!
-        }
+      <StudyingComponent />
+      {
+        //오오오오오 신기하다 된다!!!!!!!!!!!
+      }
 
-        <div id="product-list">
-          {products.map(function (product, index) {
-            return (
-              <div className="product-card">
-                <Link className="product-link" to={`/products/${index}`}>
-                  <div>
-                    <img className="product-img" src={product.imageUrl} />
-                  </div>
-                  <div className="product-contents">
-                    <span className="product-name">{product.name}</span>
-                    <span className="product-price">{product.price}원</span>
-                    <span className="product-seller">
-                      <img
-                        className="product-avatar"
-                        src="images/icons/avatar.png"
-                      />
-                      <span>{product.seller}</span>
-                    </span>
-                  </div>
-                </Link>
-              </div>
-            );
-          })}
-        </div>
+      <div id="product-list">
+        {products.map(function (product, index) {
+          return (
+            <div className="product-card">
+              <Link className="product-link" to={`/products/${product.id}`}>
+                <div>
+                  <img className="product-img" src={product.imageUrl} />
+                </div>
+                <div className="product-contents">
+                  <span className="product-name">{product.name}</span>
+                  <span className="product-price">{product.price}원</span>
+                  <span className="product-seller">
+                    <img
+                      className="product-avatar"
+                      src="images/icons/avatar.png"
+                    />
+                    <span>{product.seller}</span>
+                  </span>
+                </div>
+              </Link>
+            </div>
+          );
+        })}
       </div>
-      <div id="footer"></div>
     </div>
   );
 }
